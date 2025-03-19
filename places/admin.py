6e005fd4e40c3a79575d6d4b076e01eb8edaed19
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-03-19 20:18:23 +0500

    Update admin.py

--- places/admin.py
+++ places/admin.py
@@ -1,28 +1,44 @@
+from .models import Place, Image
+
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Place, Image
-from adminsortable2.admin import SortableAdminMixin, SortableTabularInline
 
 
-class ImageInline(SortableTabularInline):
+class ImageInline(admin.TabularInline):
     model = Image
     extra = 10
     readonly_fields = ['image_preview']
+    raw_id_fields = ['place']
 
     def image_preview(self, obj):
-
         try:
             return format_html(
-                '<img src="{url}" width="auto;" height="200px" />'.format(
-                    url=obj.image.url,
-                    width=obj.image.width,
-                    height=obj.image.height,
-                )
+                '<img src="{}" width="auto" height="200px" />',
+                obj.image.url
             )
-        except Exception as e:
-            return ("Фото отсутствует")
+        except Exception:
+            return "Фото отсутствует"
+
 
 
 @admin.register(Place)
-class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
+class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
+
+
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['image_preview', 'place']
+    search_fields = ['place__title']
+    raw_id_fields = ['place']
+
+    def image_preview(self, obj):
+        try:
+            return format_html(
+                '<img src="{}" width="auto" height="200px" />',
+                obj.image.url
+            )
+        except Exception:
+            return "Фото отсутствует"
