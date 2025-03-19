9d8c488e2018460020c10ae1d7d4589672f4b2c6
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-03-19 20:17:39 +0500

    Update models.py

--- places/models.py
+++ places/models.py
@@ -1,32 +1,29 @@
 from django.db import models
+
 from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    place_id = models.CharField(max_length=100, null=True, unique=True)
-    title = models.CharField(max_length=150,null=True, unique=True)
-    description_short = models.TextField()
-    description_long = HTMLField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
-    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+    title = models.CharField(max_length=150, unique=True, null=True,
+                             blank=False, verbose_name="Название места")
+    short_description = models.TextField(
+        verbose_name="Короткое описание места", null=True, blank=True)
+    long_description = HTMLField(
+        verbose_name="Подробное описание места", null=True, blank=True)
+    longitude = models.FloatField(verbose_name="Долгота")
+    latitude = models.FloatField(verbose_name="Широта")
+
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        ordering = ['order']
-
 
 class Image(models.Model):
     place = models.ForeignKey(
-        Place, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(
-        upload_to='images/', default='none', verbose_name="Картинка")
-    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+        Place, on_delete=models.CASCADE, related_name='images', verbose_name="Картинки")
+    image = models.ImageField(upload_to='images/', verbose_name="Картинка")
 
     def __str__(self):
         return self.place.title
-
-    class Meta:
-        ordering = ['order']
